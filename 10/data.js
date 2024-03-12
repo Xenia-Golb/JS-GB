15c094213031e482aeb55b75e4e6be7a322b5b0e
Xenia Golbraykh <karzerin@gmail.com>
2024-03-12 21:38:15 +0300

    10

---
+++ 10/data.js
@@ -0,0 +1,26 @@
+const dataInfo = `[
+    {
+        "class": "new-container",
+        "url": "https://avavatar.ru/images/full/50/vPFI2CmwaeByeXFI.jpg",
+        "width": 500,
+        "height": 334,
+        "heading": "Lorem ipsum dolor sit amet",
+        "paragraph":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt quibusdam aperiam voluptates fuga, aliquam temporibus!" 
+    },
+    {
+        "class": "new-container",
+        "url": "https://avavatar.ru/images/full/50/mBW8qwGclr7c8pB6.jpg",
+        "width": 500,
+        "height": 334,
+        "heading": "Lorem ipsum dolor sit amet",
+        "paragraph":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt quibusdam aperiam voluptates fuga, aliquam temporibus!" 
+    },
+    {
+        "class": "new-container",
+        "url": "https://avavatar.ru/images/original/1/rmd7ZWOA4HgzHjFj.jpg",
+        "width": 500,
+        "height": 334,
+        "heading": "Lorem ipsum dolor sit amet",
+        "paragraph":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt quibusdam aperiam voluptates fuga, aliquam temporibus!" 
+    }
+]`;
---
+++ 10/script.js
@@ -0,0 +1,24 @@
+const data = JSON.parse(dataInfo);
+console.log(data);
+const container = document.querySelector('.container');
+console.log(container);
+data.forEach(element => {
+    const product = document.createElement('div');
+    const imgEl = document.createElement('img');
+    const h1El = document.createElement('h1');
+    const pEl = document.createElement('p');
+    imgEl.src = element.url;
+    imgEl.style.widht = element.widht + "px";
+    imgEl.style.height = element.height + "px";
+    h1El.textContent = element.heading;
+    pEl.textContent = element.paragraph;
+
+    container.appendChild(product);
+    product.classList.add(element.class);
+
+    product.appendChild(imgEl);
+    product.appendChild(h1El);
+    product.appendChild(pEl);
+
+});
+
