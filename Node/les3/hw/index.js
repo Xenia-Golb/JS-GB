b67047911bab46436cf084daa31f20ab2582b596
Xenia Golbraykh <karzerin@gmail.com>
2024-04-17 22:32:41 +0300

    hw3

--- Node/les3/hw/index.js
+++ Node/les3/hw/index.js
@@ -1,36 +1,33 @@
 const express = require('express');
 const app = express();
+
 const port = 8080;
 
 const path = require('path');
 const fs = require('fs');
 
 
-app.get('/', (req, res) => {
-    const pathToFileHome = path.join(__dirname, 'userCountHome.json');
-    console.log(pathToFileHome);
-    const userCountHomeData = JSON.parse(fs.readFileSync(pathToFileHome, 'utf-8'));
+const pathToFile = path.join(__dirname, 'counter.json');
+const counterData = JSON.parse(fs.readFileSync(pathToFile, 'utf-8'));
 
-    res.send(`<h1>Hello World!</h1>
-    <p>Счетчик просмотров сайта ${userCountHomeData.count} раз </p>
-    <a href="/about">About</a>`)
+app.get('/', (req, res) => {
+    counterData['/'] = counterData['/'] + 1;
+    fs.writeFileSync(pathToFile, JSON.stringify(counterData, null, 2));
+    const htmlHome = `<h1>Hello World!</h1>
+    <p>Просмотров: ${counterData['/']}</p>
+    <a href="/about">About</a>`
+    res.send(htmlHome)
 })
 
 app.get('/about', (req, res) => {
-    const pathToFileAbout = path.join(__dirname, 'userCountAbout.json');
-    console.log(pathToFileAbout);
-    const userCountAboutData = JSON.parse(fs.readFileSync(pathToFileAbout, 'utf-8'));
-
-    userCountAboutData.count = userCountAboutData.count + 1;
-
-    fs.writeFileSync(pathToFileAbout, JSON.stringify(userCountAboutData, null, 2));
-    res.send(`<h1>About!</h1>
-    <p>Счетчик просмотров сайта ${userCountHomeData.count} раз </p>
-    <a href='/'>Home</a>`)
+    counterData['/about'] = counterData['/about'] + 1;
+    fs.writeFileSync(pathToFile, JSON.stringify(counterData, null, 2));
+    const htmlAbout = `<h1>About!</h1>
+    <p>Просмотров: ${counterData['/about']}</p>
+    <a href='/'>Home</a>`
+    res.send(htmlAbout)
 });
-
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`);
 });
-
